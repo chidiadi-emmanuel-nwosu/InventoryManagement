04a9bccfc8edc3df398f098dabdc34f16573403d
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-11 21:55:34 +0100

    added a dropdown side bar menu

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "./Components/Dashboard";
+
+function App() {
+  return (
+    <div className="h-screen p-6 bg-neutral-200">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
